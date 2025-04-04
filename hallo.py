9287275b55f4e74d6cb07046709422059553746e
Lars Zinnkann <Lars.Zinnkann@kamax.com>
2025-04-04 09:20:27 +0200

    changed stuff

--- hallo.py
+++ hallo.py
@@ -6,8 +6,8 @@ def begruessung(name:str):
 def tschuess(name:str):
 	print("Auf Wiedersehen,", name, "!")
 
-def tschuess(name:str):
-	print("Auf Wiedersehen,", name, "!")
+def saytime():
+print("Es ist "+str(datetime.datetime.now().strftime('%H:%M:%S')))
 
 name = input("Bitte geben Sie unbedingt Namen ein: ")
 begruessung(name)

0b77dd5ae345b24b2f5a4fcd9f7246ef3f35c09a
Lars Zinnkann <Lars.Zinnkann@kamax.com>
2025-04-04 09:17:54 +0200

    add stuff

--- hallo.py
+++ hallo.py
@@ -1,9 +1,16 @@
+git checkout -b feature/time
+
 def begruessung(name:str):
 	print("Hallo", name, "!")
+
+def tschuess(name:str):
+	print("Auf Wiedersehen,", name, "!")
+
 def tschuess(name:str):
 	print("Auf Wiedersehen,", name, "!")
 
 name = input("Bitte geben Sie unbedingt Namen ein: ")
 begruessung(name)
+saytime()
 tschuess(name)
 
